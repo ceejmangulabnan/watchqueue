d36234a348cb3654d3f449ef6e1f7a6476c259f2
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-08-20 06:38:02 +0800

    added user_dependency and removed unnecessary routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,18 +20,17 @@ BASE_URL = os.getenv("BASE_URL")
 BASE_IMG_URL = os.getenv("BASE_IMG_URL")
 
 app = FastAPI()
-app.include_router(users.router)
-# Creates Database Tables from models schema
 models.Base.metadata.create_all(bind=engine)
-
-user_dependency = Annotated[dict, Depends(get_current_user)]
-
 # Allows FastAPI to accept requests from localhost frontend
 origins = [
     "http://127.0.0.1",
     "http://localhost",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://127.0.0.1",
+    "https://localhost",
+    "https://127.0.0.1:5173",
+    "https://localhost:5173",
 ]
 
 app.add_middleware(
@@ -39,8 +38,14 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Content-Type", "Set-Cookie"],
+    allow_headers=["Content-Type", "Set-Cookie", "Authorization"],
 )
+
+
+app.include_router(users.router)
+# Creates Database Tables from models schema
+
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @app.get("/")
@@ -48,23 +53,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/test")
-async def test():
-    response = requests.get(f"{BASE_URL}/movie/popular?api_key={API_KEY}")
-    return response.json()
-
-
 @app.get("/movie/popular")
 async def get_movie_popular():
     response = requests.get(f"{BASE_URL}/movie/popular?api_key={API_KEY}")
     return response.json()
-
-
-@app.get("/protected")
-async def protected(user: user_dependency):
-    if user is None:
-        raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED, detail="Unauthorized User"
-        )
-    else:
-        return {"User": user}
