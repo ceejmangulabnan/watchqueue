197a1dd64ae1079b242e28bfaac76ad58d3f9611
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-08-26 07:40:40 +0800

    styled movie items into grid

---
+++ README.md
@@ -0,0 +1,5 @@
+# watchqueue
+Watchqueue is a site for creating and managing the things you want to watch, using data from TheMovieDatabase or TMDB.
+
+
+
--- frontend/src/components/Movies/MovieItem.tsx
+++ frontend/src/components/Movies/MovieItem.tsx
@@ -9,8 +9,9 @@ interface MovieItemProps {
 const MovieItem = ({ movie }: MovieItemProps) => {
   return (
     <div className="movie-item">
-      <h2 className="movie-item__title">{movie.title}</h2>
       <img className='movie-item__poster' src={generatePosterLink(movie.poster_path)} />
+      <h2 className="movie-item__title">{movie.title}</h2>
+      <p>{movie.release_date}</p>
     </div>
   )
 }
