bc1620a8dee2030e1123473446393cce809881a9
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-10-09 16:58:29 +0800

    remove unused state and functions

--- frontend/src/components/LoginRegisterToggle/index.tsx
+++ frontend/src/components/LoginRegisterToggle/index.tsx
@@ -6,15 +6,8 @@ import { Button } from "@/components/ui/button"
 
 // Handles the switching between the login and register forms
 const LoginRegisterToggle = () => {
-
-  const [loginFormModalActive, setLoginFormModalActive] = useState(false)
   const [isLogin, setIsLogin] = useState(false)
   const [loginButtonClicked, setLoginButtonClicked] = useState(false)
-
-  const toggleLoginForm = () => {
-    console.log("Login Form Active Toggle", !loginFormModalActive)
-    setLoginFormModalActive(!loginFormModalActive)
-  }
 
   const toggleForm = () => {
     setIsLogin(!isLogin)
@@ -22,10 +15,8 @@ const LoginRegisterToggle = () => {
 
   const loginButtonToggle = () => {
     setLoginButtonClicked(true)
-    setLoginFormModalActive(true)
     setIsLogin(true)
   }
-
 
   return (
     <div>
@@ -35,13 +26,9 @@ const LoginRegisterToggle = () => {
         </DialogTrigger>
         <DialogContent>
           {
-            loginButtonClicked &&
-            (
-              isLogin
-                ?
-                <LoginForm toggleForm={toggleForm} modalActive={loginFormModalActive} toggleLoginForm={toggleLoginForm} />
-                : <RegisterForm toggleForm={toggleForm} />
-            )
+            (loginButtonClicked && isLogin)
+              ? <LoginForm toggleForm={toggleForm} />
+              : <RegisterForm toggleForm={toggleForm} />
           }
         </DialogContent>
       </Dialog>
