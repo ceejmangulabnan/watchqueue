e333b0c05a3f9bf57952ca434158ecb9d2fb852a
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-10-09 16:54:20 +0800

    add form validation and style forms

--- frontend/src/components/LoginRegisterToggle/index.tsx
+++ frontend/src/components/LoginRegisterToggle/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
-import LoginFormModal from "@/components/LoginFormModal"
-import RegisterFormModal from "@/components/RegisterFormModal"
+import LoginForm from "@/components/LoginForm"
+import RegisterForm from "@/components/RegisterForm"
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
 
 // Handles the switching between the login and register forms
 const LoginRegisterToggle = () => {
@@ -27,15 +29,22 @@ const LoginRegisterToggle = () => {
 
   return (
     <div>
-      <button className='login-form-modal__button' onClick={loginButtonToggle}>
-        Login
-      </button>
-
-      {loginButtonClicked && (
-        isLogin
-          ? <LoginFormModal toggleForm={toggleForm} modalActive={loginFormModalActive} toggleLoginForm={toggleLoginForm} />
-          : <RegisterFormModal toggleForm={toggleForm} />
-      )}
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button onClick={loginButtonToggle}>Login</Button>
+        </DialogTrigger>
+        <DialogContent>
+          {
+            loginButtonClicked &&
+            (
+              isLogin
+                ?
+                <LoginForm toggleForm={toggleForm} modalActive={loginFormModalActive} toggleLoginForm={toggleLoginForm} />
+                : <RegisterForm toggleForm={toggleForm} />
+            )
+          }
+        </DialogContent>
+      </Dialog>
     </div >
   )
 }
