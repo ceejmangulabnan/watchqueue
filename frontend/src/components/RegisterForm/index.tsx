f60b14c037e32e438991d759e4d54bb0679a4fc0
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-10-11 18:47:18 +0800

    log user in on successful register

--- frontend/src/components/RegisterForm/index.tsx
+++ frontend/src/components/RegisterForm/index.tsx
@@ -13,6 +13,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog"
 import useAxiosPrivate from "@/hooks/useAxiosPrivate"
+import { useAuth } from "@/hooks/useAuth"
 
 interface RegisterFormProps {
   toggleForm: () => void
@@ -20,6 +21,7 @@ interface RegisterFormProps {
 
 const RegisterForm = ({ toggleForm }: RegisterFormProps) => {
   const axiosPrivate = useAxiosPrivate()
+  const { auth, setAuth } = useAuth()
   const registerFormSchema = z.object({
     username: z.string().min(4).regex(/^[a-zA-Z0-9]{4,}$/, 'Username must be at least 4 characters long without special characters'),
     password: z.string().min(8).regex(/^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/, 'Password must include at least 1 uppercase and lowercase letter, 1 number, and 1 special character'),
@@ -42,6 +44,14 @@ const RegisterForm = ({ toggleForm }: RegisterFormProps) => {
   const onSubmit = async (registerValues: z.infer<typeof registerFormSchema>) => {
     try {
       await axiosPrivate.post('/users/register', registerValues)
+      const login = await axiosPrivate.post('/users/token', { username: registerValues.username, password: registerValues.password }, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      const userData = await axiosPrivate.get('/users/me')
+
+      setAuth({ ...auth, accessToken: login.data.access_token, id: userData.data.id, username: userData.data.username })
     } catch (error) {
       console.error(error)
     } finally {
