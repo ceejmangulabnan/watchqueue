b433f4ba4f1ac7e23279cf20adf2bf397a57e576
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-10-13 18:07:18 +0800

    change cors settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,14 +30,21 @@ origins = [
     "https://watchqueue.netlify.app",
 ]
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
+#     allow_headers=["*"]
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 
 app.include_router(users.router)
 app.include_router(watchlists.router)
