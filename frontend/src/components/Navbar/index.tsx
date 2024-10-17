874d274b6cbbdcb953dd32963c974c06d943b43d
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-10-17 17:18:10 +0800

    fix rendering incorrect navbar ui based on auth state
    - added skeleton for loading state

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,40 +1,41 @@
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import useAxiosPrivate from '@/hooks/useAxiosPrivate'
 import { useAuth } from '@/hooks/useAuth'
 import useRefreshUser from '@/hooks/useRefreshUser'
 import LoginRegisterToggle from '@/components/LoginRegisterToggle'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const Navbar = () => {
   const axiosPrivate = useAxiosPrivate()
   const { auth, setAuth } = useAuth()
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [loading, setLoading] = useState(true)
   const refreshUser = useRefreshUser()
   const navigate = useNavigate()
 
-  useLayoutEffect(() => {
-    refreshUser()
-  }, [refreshUser])
-
   useEffect(() => {
-    const checkUserData = async () => {
-      // Check userData values if not null
-      if (auth && !Object.values(auth).includes(null)) {
-        setIsLoggedIn(true)
+    const initAuthCheck = async () => {
+      try {
+        await refreshUser()
+      } catch (error) {
+        console.error("Error refreshing user data", error)
+      } finally {
+        setLoading(false)
       }
     }
-    checkUserData()
-  }, [auth])
+    initAuthCheck()
+  }, [refreshUser])
 
   const handleLogout = async () => {
     const response = await axiosPrivate.post("/users/logout")
     if (response.status == 200) {
       setAuth({ ...auth, id: null, username: null, accessToken: null })
-      setIsLoggedIn(false)
       navigate("/", { replace: true })
     }
   }
+
+  const isAuthed = auth && !Object.values(auth).includes(null)
 
   return (
     <div className='z-10 fixed top-0 left-0 right-0 flex justify-between items-center px-8 py-3 text-md font-medium shadow-md bg-white'>
@@ -42,32 +43,37 @@ const Navbar = () => {
         <h1 className='text-2xl font-semibold'>watchqueue</h1>
       </Link>
       <nav>
-        <ul className='flex items-center gap-3'>
-          {
-            isLoggedIn ? (
-              <ul className='flex items-center gap-3'>
-                <li>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li>
-                  <Link to='/profile'>Profile</Link>
-                </li>
-                <li>
-                  <Button variant={"destructive"} onClick={handleLogout}>Logout</Button>
-                </li>
-              </ul>
-            ) : (
-              <ul className='flex items-center gap-3'>
-                <li>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li>
-                  <LoginRegisterToggle />
-                </li>
-              </ul>
-            )
-          }
-        </ul>
+        {loading ? (
+          // Render skeleton while fetching updated auth state
+          <ul className='flex items-center gap-3'>
+            <li><Skeleton className="h-9 w-16" /></li>
+            <li><Skeleton className="h-9 w-16" /></li>
+            <li><Skeleton className="h-9 w-20" /></li>
+          </ul>
+        ) : isAuthed ? (
+          // If user is authenticated, show logged-in navigation
+          <ul className='flex items-center gap-3'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+            <li>
+              <Button variant="destructive" onClick={handleLogout}>Logout</Button>
+            </li>
+          </ul>
+        ) : (
+          // If not authed, show login button
+          <ul className='flex items-center gap-3'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <LoginRegisterToggle />
+            </li>
+          </ul>
+        )}
       </nav>
     </div>
   )
--- frontend/src/hooks/useRefreshUser.tsx
+++ frontend/src/hooks/useRefreshUser.tsx
@@ -3,35 +3,31 @@ import { useAuth } from "./useAuth"
 import axios from "../api/axios"
 import { useCallback } from 'react'
 
-// Should run before site loads
 const useRefreshUser = () => {
   const { setAuth } = useAuth()
 
   // UseCallback to handle recreate only if auth state changes
   const refreshUser = useCallback(async () => {
     // Refresh access token and populate auth context on inital render / reload
-    try {
-      // Does a check for an existing refresh token, if not found returns 401
-      const refresh = await axios.get('/users/refresh', {
-        withCredentials: true
-      })
-      const refreshData = await refresh.data
-      const user = await axios.get('/users/me', {
-        headers: {
-          Authorization: `Bearer ${refreshData.access_token}`
-        }
-      })
-      const userData = await user.data
+    // Does a check for an existing refresh token, if not found returns 401
+    const refresh = await axios.get('/users/refresh', {
+      withCredentials: true
+    })
+    const refreshData = await refresh.data
+    const user = await axios.get('/users/me', {
+      headers: {
+        Authorization: `Bearer ${refreshData.access_token}`
+      }
+    })
+    const userData = await user.data
 
-      // If fetching is successful, then update the auth context
-      setAuth(prevAuth => ({ ...prevAuth, accessToken: refreshData.access_token, id: userData.id, username: userData.username }))
+    // If fetching is successful, then update the auth context
+    setAuth(prevAuth => ({ ...prevAuth, accessToken: refreshData.access_token, id: userData.id, username: userData.username }))
 
-    } catch (error) {
-      console.log("Error fetching auth on intial render/reload. Refresh token not found, user must log in", error)
-    }
+    return userData
+
   }, [setAuth])
 
-  // This function should be run inside a useLayoutEffect
   return refreshUser
 }
 
