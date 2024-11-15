5f4a9504109e79c278dbc021a877039747805956
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-11-15 16:51:14 +0800

    added search movie and tv routes

--- backend/app/routers/search.py
+++ backend/app/routers/search.py
@@ -11,6 +11,17 @@ API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")
 BASE_IMG_URL = os.getenv("BASE_IMG_URL")
 
+@router.get("/movie")
+async def search_movie(query: str):
+    response = requests.get(f"{BASE_URL}/search/movie?query={query}&api_key={API_KEY}")
+    return response.json()
+
+@router.get("/tv")
+async def search_show(query: str):
+    response = requests.get(f"{BASE_URL}/search/tv?query={query}&api_key={API_KEY}")
+    return response.json()
+
+
 @router.get("/multi")
 async def search_multi(query: str):
     response = requests.get(f"{BASE_URL}/search/multi?query={query}&api_key={API_KEY}")
