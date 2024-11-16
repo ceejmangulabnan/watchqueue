9be52398624ae96f7df02aac26fb20b3b067dde9
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-11-16 09:08:41 +0800

    search bar handles search params
    search bar only handles routing to results page and adding search query
    to urlsearchparams

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -1,25 +1,10 @@
 import { Input } from '@/components/ui/input'
-import { useQuery } from '@tanstack/react-query'
-import useAxiosPrivate from '@/hooks/useAxiosPrivate'
 import { useState } from 'react'
-import { MovieDataQuery } from '@/types/MovieTypes'
+import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
-  const axiosPrivate = useAxiosPrivate()
   const [searchQuery, setSearchQuery] = useState<string>('')
-
-  const fetchSearchMovie = async () => {
-    const response = await axiosPrivate.get(`/search/movie?query=${searchQuery}`)
-    return response.data as MovieDataQuery
-  }
-
-  const { data: movieSearchResults, refetch } = useQuery({
-    queryKey: ["movieSearchResults", searchQuery],
-    queryFn: fetchSearchMovie,
-    enabled: false
-  })
-  console.log(movieSearchResults)
-
+  const navigate = useNavigate()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value)
@@ -27,11 +12,13 @@ const SearchBar = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    refetch()
+    if (searchQuery.trim()) {
+      navigate(`/search/movie?query=${encodeURIComponent(searchQuery)}`)
+    }
   }
 
   return (
-    <div id='searchBar' className=''>
+    <div>
       <form onSubmit={handleSubmit}>
         <Input placeholder='Search...' className='w-[400px]' value={searchQuery} onChange={handleChange} />
       </form>
