adc1d62225ef294efd32a4671dfdb8507d57c426
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-12-06 09:41:01 +0800

    search category options

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -1,9 +1,13 @@
 import { Input } from '@/components/ui/input'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { ChevronDown } from 'lucide-react'
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState<string>('')
+  const [searchCategory, setSearchCategory] = useState<string>('All')
   const navigate = useNavigate()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +24,28 @@ const SearchBar = () => {
   return (
     <div className='flex-1 min-w-[100px] max-w-[600px]'>
       <form onSubmit={handleSubmit}>
-        <Input placeholder='Search...' className='' value={searchQuery} onChange={handleChange} />
+        <div className='flex items-center shadow-sm border border-input rounded-sm'>
+          <DropdownMenu>
+            <DropdownMenuTrigger className=''>
+              <Button variant={"outline"} className='border-none shadow-none w-28 justify-between'>{searchCategory}
+                <ChevronDown size={20} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-28'>
+              <DropdownMenuItem onClick={() => setSearchCategory('All')}>
+                All
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setSearchCategory('Movies')}>
+                Movies
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setSearchCategory('Shows')}>
+                Shows
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <Input placeholder='Search watchqueue...' className='border-none shadow-none' value={searchQuery} onChange={handleChange} />
+        </div>
       </form>
     </div>
   )
