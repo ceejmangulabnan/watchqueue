c88c7531c86a326b2d5ab369634d02f28b5aebec
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-12-09 14:05:19 +0800

    created scrollable list component and added popular tv shows to landing page

--- backend/app/routers/tv.py
+++ backend/app/routers/tv.py
@@ -13,6 +13,11 @@ BASE_URL = os.getenv("BASE_URL")
 BASE_IMG_URL = os.getenv("BASE_IMG_URL")
 
 
+@router.get("/popular")
+async def get_popular_tv():
+    response = requests.get(f"{BASE_URL}/tv/popular?api_key={API_KEY}")
+    return response.json()
+
 @router.get("/{tv_id}")
 async def get_tv_details(tv_id: int):
     response = requests.get(f"{BASE_URL}/tv/{tv_id}?api_key={API_KEY}")
--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,9 +1,37 @@
-import PopularMovies from '@/components/Movies/PopularMovies'
+import useAxiosPrivate from '@/hooks/useAxiosPrivate'
+import { useQuery } from '@tanstack/react-query'
+import { MovieDataQuery } from '@/types/MovieTypes'
+import { TvDataQuery } from '@/types/TvTypes'
+import ScrollableList from '@/components/ScrollableList'
 
 const LandingPage = () => {
+  const axiosPrivate = useAxiosPrivate()
+
+  const fetchPopularMovies = async () => {
+    const response = await axiosPrivate.get('/movies/popular')
+    return response.data as MovieDataQuery
+  }
+
+  const { data: popularMovies, isLoading: isPopularMoviesLoading } = useQuery({ queryKey: ['popularMovies'], queryFn: fetchPopularMovies })
+
+  const fetchPopularTv = async () => {
+    const response = await axiosPrivate.get('/tv/popular')
+    return response.data as TvDataQuery
+  }
+
+  const { data: popularTv, isLoading: isPopularTvLoading } = useQuery({ queryKey: ['popularTv'], queryFn: fetchPopularTv })
+
   return (
     <div className='mx-10 md:mx-20 my-10'>
-      <PopularMovies />
+      <div className='mx-auto xl:max-w-[1400px] 2xl:max-w-[1600px]'>
+        <h3 className="text-base md:text-xl font-semibold py-4 ">Popular Movies</h3>
+        {(popularMovies && !isPopularMoviesLoading) &&
+          <ScrollableList scrollableItems={popularMovies} isDataLoading={isPopularMoviesLoading} />}
+
+        <h3 className="text-base md:text-xl font-semibold py-4 ">Popular TV Shows</h3>
+        {(popularTv && !isPopularTvLoading) &&
+          <ScrollableList scrollableItems={popularTv} isDataLoading={isPopularTvLoading} />}
+      </div>
     </div>
   )
 }
