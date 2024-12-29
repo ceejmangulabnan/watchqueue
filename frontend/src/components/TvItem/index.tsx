45c588e881bd09afbc2df12911e43582c29b036f
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-12-30 07:50:23 +0800

    resolve build conflicts

--- frontend/src/components/TvItem/index.tsx
+++ frontend/src/components/TvItem/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Card, CardTitle, CardFooter, CardDescription } from '@/components/ui/card'
 import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
