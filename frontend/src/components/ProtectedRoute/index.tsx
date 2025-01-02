0d7617da706beb0e284543ec7c1d5b481c9b086a
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-01-03 07:22:16 +0800

    loading spinner

--- frontend/src/components/ProtectedRoute/index.tsx
+++ frontend/src/components/ProtectedRoute/index.tsx
@@ -1,3 +1,4 @@
+import LoadingSpinner from '@/components/ui/spinner'
 import { useAuth } from "@/hooks/useAuth"
 import { useNavigate, Outlet } from "react-router-dom"
 
@@ -5,17 +6,12 @@ const ProtectedRoute = () => {
   const { auth, isAuthLoading, logout } = useAuth()
   const navigate = useNavigate()
 
-  if (isAuthLoading) {
-    return <div>Loading...</div>
-  }
-
   const isAuthenticated = auth.accessToken !== null &&
     auth.username !== null &&
     auth.id !== null
 
   if (isAuthLoading) {
-    // TODO: Replace with proper loading screen / loading spinner
-    return <div>Loading...</div>
+    return <LoadingSpinner />
   }
 
   if (!isAuthenticated) {
