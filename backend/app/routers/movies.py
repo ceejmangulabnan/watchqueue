3aaae0896693aa5231f31e53e7d8e2d872f3bf39
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2026-05-03 18:41:30 +0800

    feat: updated movie and tv routers to use async with httpx

--- backend/app/routers/movies.py
+++ backend/app/routers/movies.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-import os
 from dotenv import load_dotenv
-import requests
+import httpx
+import os
 
 router = APIRouter(prefix="/movies")
 
@@ -11,32 +11,30 @@ API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")
 BASE_IMG_URL = os.getenv("BASE_IMG_URL")
 
-
-@router.get("/")
-async def movies():
-    return {"message": "movies"}
+client = httpx.AsyncClient(
+    headers={"Authorization": f"Bearer {API_KEY}"},
+)
 
 
 @router.get("/popular")
 async def get_movie_popular():
-    response = requests.get(f"{BASE_URL}/movie/popular?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/movie/popular")
     return response.json()
+
 
 @router.get("/top_rated")
 async def get_movie_top_rated():
-    response = requests.get(f"{BASE_URL}/movie/top_rated?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/movie/top_rated")
     return response.json()
+
 
 @router.get("/{movie_id}")
 async def get_movie_details(movie_id: int):
-    response = requests.get(f"{BASE_URL}/movie/{movie_id}?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/movie/{movie_id}")
     return response.json()
+
 
 @router.get("/{movie_id}/recommendations")
 async def get_movie_recommendations(movie_id: int):
-    response = requests.get(f"{BASE_URL}/movie/{movie_id}/recommendations?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/movie/{movie_id}/recommendations")
     return response.json()
-
-
-
-
--- backend/app/routers/tv.py
+++ backend/app/routers/tv.py
@@ -1,7 +1,7 @@
 # Tv Router
 from fastapi import APIRouter
 from dotenv import load_dotenv
-import requests
+import httpx
 import os
 
 router = APIRouter(prefix="/tv")
@@ -12,23 +12,30 @@ API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")
 BASE_IMG_URL = os.getenv("BASE_IMG_URL")
 
+client = httpx.AsyncClient(
+    headers={"Authorization": f"Bearer {API_KEY}"},
+)
+
 
 @router.get("/popular")
 async def get_tv_popular():
-    response = requests.get(f"{BASE_URL}/tv/popular?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/tv/popular")
     return response.json()
+
 
 @router.get("/top_rated")
 async def get_tv_top_rated():
-    response = requests.get(f"{BASE_URL}/tv/top_rated?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/tv/top_rated")
     return response.json()
+
 
 @router.get("/{tv_id}")
 async def get_tv_details(tv_id: int):
-    response = requests.get(f"{BASE_URL}/tv/{tv_id}?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/tv/{tv_id}")
     return response.json()
+
 
 @router.get("/{tv_id}/recommendations")
 async def get_tv_recommendations(tv_id: int):
-    response = requests.get(f"{BASE_URL}/tv/{tv_id}/recommendations?api_key={API_KEY}")
+    response = await client.get(f"{BASE_URL}/tv/{tv_id}/recommendations")
     return response.json()
